b5a3a7fce32b70e10ad2312ea4712dabf22981b2
Gabor V <gabor.w.varga@gmail.com>
2017-03-22 14:15:01 +0100

    added sum_fn.py

--- week-02/day-03/sum_fn.py
+++ week-02/day-03/sum_fn.py
@@ -1,5 +1,12 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
 
-def sum():
-	
+
+
+def summ(number):
+    for x in range(1,number):
+        number = x + number
+    print(number)
+
+
+summ(5)
