5843a02beb412316e261d11abdb82ed5086a0fd3
Gabor V <gabor.w.varga@gmail.com>
2017-03-22 13:14:34 +0100

    added  double-items.py

---
+++ week-02/day-03/diagonal-matrix.py
@@ -0,0 +1,13 @@
+# - Create (dynamically) a two dimensional list
+#   with the following matrix. Use a loop!
+#
+#   1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1
+#
+# - Print this two dimensional list to the output
+
+
+for x in range(1,4):
+    
--- week-02/day-03/sum_fn.py
+++ week-02/day-03/sum_fn.py
@@ -1,2 +1,5 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
+
+def sum():
+	
