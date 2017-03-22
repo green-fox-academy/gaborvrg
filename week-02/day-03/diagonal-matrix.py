30bf90d9106ef3341d597b35514481a59bd8a00b
Gabor V <gabor.w.varga@gmail.com>
2017-03-22 17:44:23 +0100

    vege

--- week-02/day-03/diagonal-matrix.py
+++ week-02/day-03/diagonal-matrix.py
@@ -8,15 +8,15 @@
 #
 # - Print this two dimensional list to the output
 
-matr = 4
-row = []
+size = 5
+matrix = []
 
 
-for y in range(0,matr):
+for y in range(0,size):
     col = []
-    for x in range(0,matr):
+    for x in range(0,size):
         col.append(0)
     col[y] = 1
-    row.append(col)
 
-print(row)
+    matrix.append(col)
+print(matrix)
