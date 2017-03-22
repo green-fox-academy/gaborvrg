d69f91009dbed11d26c74005e6e7de9c2fbec105
Gabor V <gabor.w.varga@gmail.com>
2017-03-22 16:57:55 +0100

    added diagonal-matrix in khm... linear mode :)

--- week-02/day-03/diagonal-matrix.py
+++ week-02/day-03/diagonal-matrix.py
@@ -8,6 +8,15 @@
 #
 # - Print this two dimensional list to the output
 
+matr = 4
+row = []
 
-for x in range(1,4):
-    
+
+for y in range(0,matr):
+    col = []
+    for x in range(0,matr):
+        col.append(0)
+    col[y] = 1
+    row.append(col)
+
+print(row)
---
+++ week-02/day-03/printer.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer(*arg):
+    print(arg)
+
+printer('sok', 'keves', 'akarmi', 'szalami', 16 )
