2dd16be91d459550d4d50b91d18802eff42138cd
Gabor V <gabor.w.varga@gmail.com>
2017-03-23 16:36:39 +0100

    added isinlist.py

---
+++ week-02/day-04/isinlist_06.py
@@ -0,0 +1,24 @@
+# Check if list contains all of the following elements: 4,8,12,16
+# Create a function that accepts listOfNumbers as an input
+# it should return "true" if it contains all, otherwise print "false"
+
+listOfNumbers = [2, 4, 6, 8, 10, 12, 14]
+
+test = [4, 8, 12, 16]
+
+def numbercheck(lisst, find):
+    out = False
+    test_len = 0
+
+    for l in lisst:
+        for f in find:
+            if l == f:
+                test_len += 1               
+                if test_len == len(find):
+                    out = True
+    return out
+
+it_contains_all = numbercheck(listOfNumbers,test)
+
+print(it_contains_all)
+
