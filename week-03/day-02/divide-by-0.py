bfd63901b3b164bf1d2b9e4c37e8fdbdfe5f29a5
Gabor V <gabor.w.varga@gmail.com>
2017-03-28 15:20:41 +0200

    added divide-by-0.py first good version, without function

--- week-03/day-02/divide-by-0.py
+++ week-03/day-02/divide-by-0.py
@@ -2,3 +2,24 @@
 # divides ten with it,
 # and prints the result.
 # it should print "fail" if the parameter is 0
+
+# try:
+#     f = open(arg, 'r')
+# except IOError:
+#     print('cannot open', arg)
+
+while True:
+    try:
+        number = int(input("Type an integer: "))
+        print(10 / number)
+        break
+
+    except ZeroDivisionError:
+        print("Don't type 0(zero), pls. Try again!")
+
+    except ValueError:
+        print("Don't type string, pls.  Try again!")
+
+
+
+
