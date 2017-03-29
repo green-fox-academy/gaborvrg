eddbffeb7d5aca6d2a8362c46017819443f0fa51
Gabor V <gabor.w.varga@gmail.com>
2017-03-29 10:55:26 +0200

    added horizontal-lines-05.py

--- week-03/day-03/horizontal-lines-05.py
+++ week-03/day-03/horizontal-lines-05.py
@@ -1,23 +1,26 @@
-from tkinter import *
 
 # create a line drawing function that takes 2 parameters:
 # the x and y coordinates of the line's starting point
 # and draws a 50 long horizontal line from that point.
 # draw 3 lines with that function.
 
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+    
+
 def drawing():
 
-    x, y = int(input("Type two integer for the starting points with space: ").split())
-    
-    root = Tk()
+    x, y = input("Type two integer for the starting points with space: ").split()
 
-    canvas = Canvas(root, width='300', height='300')
-    canvas.pack()
+    x_int = int(x)
+    y_int = int(y)
 
-    # x_end = str(x + 50)
-    # y_end = y
 
-    canvas.create_line(x, y, x + 50, y, fill='red', width=3)
+    canvas.create_line(x_int, y_int, x_int + 50, y_int, fill='red', width=3)
 
     root.mainloop()
 
