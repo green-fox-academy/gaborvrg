5df90fcabc2d1dbae54342b78600caa745fb2e7a
Gabor V <gabor.w.varga@gmail.com>
2017-03-29 16:59:54 +0200

    update perfect solution to purple-steps-3d-12.py

--- week-03/day-03/purple-steps-3d-12.py
+++ week-03/day-03/purple-steps-3d-12.py
@@ -8,20 +8,30 @@ root = Tk()
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-x = 0
-y = 0
-xx = 0
-yy = 0
+x = 10
+y = 10
+xx = 20
+yy = 20
+
 
 for a in range(10, 70, 10):
- 
-    print(a)
+    print(x, y, xx, yy)
+    x = x + a
+    y = y + a
+    xx = x + a + 10
+    yy = y + a + 10
 
+
+    canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
     
 
-    canvas.create_rectangle(x + a, y + a, xx + a, yy + a, width = 1)
 
-    x = a
-    y = a
+# canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
+# canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
+# canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
+# canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
+# canvas.create_rectangle(x , y, xx, yy, width = 1, fill = 'purple')
+
+
 
 root.mainloop()
