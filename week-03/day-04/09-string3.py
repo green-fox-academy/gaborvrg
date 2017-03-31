220ef21c95f2784f9ebdd8d4184a377706fd467e
Gabor V <gabor.w.varga@gmail.com>
2017-03-31 10:00:51 +0200

    added 09 exercise finished

---
+++ week-03/day-04/09-string3.py
@@ -0,0 +1,22 @@
+# Given a string, compute recursively a new string where all the
+# adjacent chars are now separated by a "*".
+
+text = "The axa to yay xoxo"
+
+
+def string_change(input):
+    # print(input)
+
+    if input == "":
+        return input
+
+    # if input[0] :
+    #     # print(input)
+    #     return "" + string_change(input[1:]) 
+
+    else:
+        print(input)
+        # print(input[0])
+        return input[0] + " * " + string_change(input[1:]) 
+
+print(string_change(text))
