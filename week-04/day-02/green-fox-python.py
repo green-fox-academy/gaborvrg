5ea3f965fb093844002774e93c6078ff88182912
Gabor V <gabor.w.varga@gmail.com>
2017-04-04 14:12:23 +0200

    updated and tested green-fox-python.py with good result

--- week-04/day-02/green-fox-python.py
+++ week-04/day-02/green-fox-python.py
@@ -51,25 +51,25 @@ class Person():
 
 class Student(Person):
 
-    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', previous_organization = "The School of Life", skip_days = 0):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', previous_organization = "The School of Life", skipped_days = 0):
         super().__init__(name, age, gender)
         self.previous_organization = previous_organization
-        self.skip_days = skip_days
+        self.skipped_days = skipped_days
 
     def get_goal(self):
         print("Be a junior software developer.")
 
     def introduce(self):
-        print("Hi, I'm", self.name, " a ",self.age, " year old ", self.gender, "from ", self.previous_organization, "who skipped",  self.skip_days, "days from the course already.")
+        print("Hi, I'm", self.name, " a ",self.age, " year old ", self.gender, "from ", self.previous_organization, "who skipped",  self.skipped_days, "days from the course already.")
 
-    def skipped_days(self, number_of_days):
-        self.skip_days += number_of_days
+    def skip_days(self, number_of_days):
+        self.skipped_days += number_of_days
         
 # jane = Student()
 # jane.get_goal()
 # jane.introduce()
 # jane.skipped_days(3)
-# print(jane.skip_days)
+# print(jane.skipped_days)
 
 
 # Create a Mentor class that has the same fields and methods as the Person class, and has the following additional
@@ -102,7 +102,122 @@ class Mentor(Person):
 # jane.introduce()
 
 
+# Create a Sponsor class that has the same fields and methods as the Personclass, and has the following additional
 
+# fields:
+    # company: name of the company
+    # hired_students: number of students hired
+
+# method:
+    # introduce(): "Hi, I'm name, a age year old gender who represents company and hired hired_students students so far."
+    # hire(): increase hired_students by 1
+    # get_goal(): prints out "Hire brilliant junior software developers."
+
+# The Sponsor class has the following constructors:
+    # Sponsor(name, age, gender, company): beside the given parameters, it sets hired_students to 0
+    # Sponsor(): sets name to Jane Doe, age to 30, gender to female, company to Google and hired_students to 0
+
+
+class Sponsor(Person):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', company = 'Google', hired_students = 0):
+        super().__init__(name, age, gender)
+        self.company = company
+        self.hired_students = hired_students
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + ", a " + str(self.age) + " year old " + self.gender + ", who represents " + self.company + " and hired " + str(self.hired_students) + " students so far.")
+
+    def get_goal(self):
+            print("Hire brilliant junior software developers.")
+
+    def hire(self):
+        self.hired_students += 1
+
+
+# jane = Sponsor()
+# jane.hire()
+# jane.introduce()
+# jane.get_goal()
+# print(jane.hired_students)
+
+
+# Create a LagopusClass class that has the following
+
+# fields:
+    # class_name: the name of the class
+    # students: a list of Students
+    # mentors: a list of Mentors
+
+# methods:
+    # addStudent(Student): adds the given Student to students list
+    # addMentor(Mentor): adds the given Mentor to mentors list
+    # info(): prints out "Lagopus className class has students.size students and mentors.size mentors."
+
+# The LagopusClass class has the following constructors:
+    # LagopusClass(class_name): beside the given parameter, it sets students and mentors as empty lists
+
+
+class LagopusClass():
+    def __init__(self, class_name = '', students = [], mentors = []):
+        # super(LagoplusClass, self).__init__()
+        self.class_name = class_name
+        self.students = students
+        self.mentors = mentors
+
+
+    def add_student(self, student):
+        self.students.append(student)
+
+
+    def add_mentor(self, mentor):
+        self.mentors.append(mentor)
+
+
+    def info(self):
+        print("Lagopus ", self.class_name ," class has ", len(self.students), " students and " ,len(self.mentors) ," mentors.")
+        
+
+
+
+
+
+people = []
+
+mark = Person('Mark', 46, 'male')
+people.append(mark)
+jane = Person()
+people.append(jane)
+john = Student('John Doe', 20, 'male', 'BME')
+people.append(john)
+student = Student()
+people.append(student)
+gandhi = Mentor('Gandhi', 148, 'male', 'senior')
+people.append(gandhi)
+mentor = Mentor()
+people.append(mentor)
+sponsor = Sponsor()
+elon = Sponsor('Elon Musk', 46, 'male', 'SpaceX')
+student.skip_days(3)
+
+for i in range(5):
+    elon.hire()
+
+for i in range(3):
+    sponsor.hire()
+
+for member in people:
+    member.introduce()
+    member.get_goal()
+
+badass = LagopusClass('BADA55')
+badass.add_student(student);
+badass.add_student(john);
+badass.add_mentor(mentor);
+badass.add_mentor(gandhi);
+badass.info();
+
+
+        
 
 
 
