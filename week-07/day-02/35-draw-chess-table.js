b0efb914cd4e6e00e09b7be6ad813a40f06771db
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 20:55:34 +0200

    update 35.js with changable chess table size

---
+++ week-07/day-02/35-draw-chess-table.js
@@ -0,0 +1,42 @@
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+//
+
+var lineCount = 6
+
+// old version
+
+// for (var i = 1; i < lineCount + 1; i++) {
+//     if (i % 2 === 1) {
+//         console.log('% % % %');
+//     } else {
+//         console.log(' % % % %');
+//     }
+// }
+
+// newer version
+
+for (var i = 0; i < lineCount; i++) {
+    // console.log(i+1);
+    for (var j = 0; j < lineCount; j++) {       
+    }
+    if (i % 2 === 0) {
+        console.log('% '.repeat(j));
+    } else {
+        console.log(' %'.repeat(j));
+    }
+}
+
+
+
+
+
+
