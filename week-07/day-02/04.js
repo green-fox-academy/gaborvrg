3dbf38397f6102191322fc3ce554ee052fbfe546
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 13:43:42 +0200

    added 04.js

---
+++ week-07/day-02/04.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+console.log('My name is: Gabor');
+console.log('My age is 43');
+console.log('I\'m 1.87m tall');
