e70efb93b42a26f5c1375bcfd8629fde4716e104
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 14:28:03 +0200

    added 08.js with conditional of married status-update

---
+++ week-07/day-02/08-define-basic-info.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var myName = 'Gabor Varga';
+var myAge = 43;
+var myHeight = 1.84;
+var amImarried = false;
+
+console.log('My name is :' + myName );
+console.log('I\'m ' + myAge + 'years old.');
+console.log('My height is ' + myHeight);
+
+if (amImarried) {
+    console.log('I\' married.');
+} else {
+    console.log('I\'m not married.');
+}
