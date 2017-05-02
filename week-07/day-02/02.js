aef3c9ec703654d0365731719af4ddd145ad0456
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 13:30:50 +0200

    added 02.js

---
+++ week-07/day-02/02.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello, Fanni !');
+console.log('Hello, Bence !');
+console.log('Hello, Simon !');
