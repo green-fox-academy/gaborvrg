f5f31ef65b8edec44dac4e96909801b87d14caa0
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 14:58:21 +0200

    added 12.js

---
+++ week-07/day-02/12-swap.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var a = 123;
+var b = 526;
+// Swap the values of these variables
+
+var c = a;
+a = b;
+b = c;
+
+// another solution : b = [a, a = b][0];
+
+// b = [a, a = b][0]
+
+
+console.log(a);
+console.log(b);
