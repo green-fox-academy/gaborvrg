07abb51971f8ddb6d7ea219eb575b25f091b057c
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 14:34:53 +0200

    added 09.js

---
+++ week-07/day-02/09-bmi.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var massInKg = 81.2;
+var heightInM = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+console.log('This body parameters\' BMI is: ' + (massInKg / Math.pow(heightInM,2)));
