c47c05214025ab0a33bf26d1b8e6b57b8a429122
Gabor V <gabor.w.varga@gmail.com>
2017-05-02 14:13:11 +0200

    added 06.js

--- week-07/day-02/06.js
+++ week-07/day-02/06.js
@@ -8,3 +8,14 @@
 //
 // Print the percentage of the coding hours in the semester if the average
 // work hours weekly is 52
+
+
+var semesterLengthWeeks = 17;
+var codeHoursDaily = 6;
+var workdays = 5;
+var averageWorkHoursWeekly = 52;
+
+var codingByAttendee = semesterLengthWeeks * workdays;
+
+console.log('An attendee spends ' + codingByAttendee * codeHoursDaily + ' hour in a semester with coding');
+console.log(Math.floor(100 * codeHoursDaily * workdays / averageWorkHoursWeekly));
