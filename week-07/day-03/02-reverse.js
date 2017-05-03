2fbb2136753c56cc70201abe0ae82b2189b1d398
Gabor V <gabor.w.varga@gmail.com>
2017-05-03 16:23:37 +0200

    added 02-reverse.js

--- week-07/day-03/02-reverse.js
+++ week-07/day-03/02-reverse.js
@@ -3,3 +3,8 @@
 
 var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
 
+function reverseString(str) {
+    return str.split('').reverse().join(''); 
+}
+
+console.log(reverseString(reversed))
