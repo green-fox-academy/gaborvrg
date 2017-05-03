bb682ba2cb025215dbfdd6b0368a1df5fcacc32b
Gabor V <gabor.w.varga@gmail.com>
2017-05-03 15:15:26 +0200

    added 18-diagonal-matrix.js- missing diagonal

---
+++ week-07/day-03/18-diagonal-matrix.js
@@ -0,0 +1,22 @@
+'use strict';
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+var sizeOfMatrix = 6;
+
+for (var x = 1; x < sizeOfMatrix + 1; x++) {
+    // console.log(i);
+    for (var y = 0; y < sizeOfMatrix; y++) {
+    }
+    console.log('0 '.repeat(y));
+}
