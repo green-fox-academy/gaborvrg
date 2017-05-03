5120ebbb1f86b59b7e58c63a1641f2e2bde78fb8
Gabor V <gabor.w.varga@gmail.com>
2017-05-03 16:37:52 +0200

    added 03-urlfixer.js

---
+++ week-07/day-03/03-urlfixer.js
@@ -0,0 +1,13 @@
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+var url = "https//www.reddit.com/r/nevertellmethebots";
+
+function urlFixer(str) {
+    var fixUrl = str.replace('https', 'https:');
+    return fixUrl.replace('bots', 'odds')
+}
+
+console.log(urlFixer(url));
+
