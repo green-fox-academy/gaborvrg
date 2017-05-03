4850954a650d161474c9715998f9b903a7a437a3
Gabor V <gabor.w.varga@gmail.com>
2017-05-03 14:31:53 +0200

    added 14-append-a.js

---
+++ week-07/day-03/14-append-a.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+
+var nimals = ["kuty", "macs", "cic"];
+
+var animals = nimals.map(function(elements){
+    return elements + 'a'
+})
+
+console.log(animals);
