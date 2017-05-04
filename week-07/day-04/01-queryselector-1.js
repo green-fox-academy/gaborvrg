3f49f072bdea2aeaa4af20c5b1c9a2bdcee30596
Gabor V <gabor.w.varga@gmail.com>
2017-05-04 14:16:17 +0200

    added content-1.html

--- week-07/day-04/01-queryselector-1.js
+++ week-07/day-04/01-queryselector-1.js
@@ -12,25 +12,27 @@
  //    4. store 'The King' and 'The Conceited Man'
  //    in the 'conceitedKing' variable.
  //    alert them one by one.
+
  //    5. store 'The King', 'The Conceited Man' and 'The Lamplighter'
  //    in the 'noBusiness' variable.
  //    console.log each of them.
+
+
  //    6. store 'The Businessman' in the 'allBizniss' variable.
  //    show the result in an 'alert' window.
 
-// var king = document.getElementById('b325');
-// console.log(king);
 
-// var conceited = document.getElementsByClassName('b326')[0].textContent;
-// console.log(conceited);
-
-
+// 1
 
 var king = document.querySelector('#b325').textContent;
 console.log(king);
 
+// 2
+
 var conceited = document.querySelector('.b326').textContent;
 alert(conceited);
+
+// 3
 
 var businessLamp = document.getElementsByClassName('big');
 console.log(businessLamp[0].textContent, businessLamp[1].textContent);
@@ -38,9 +40,21 @@ console.log(businessLamp[0].textContent, businessLamp[1].textContent);
 var businessLamp2 = document.querySelectorAll('.big');
 console.log(businessLamp[0].textContent, businessLamp[1].textContent);
 
+// 4
 
+var conceitedKing = document.querySelectorAll('section .asteroid');
+window.alert(conceitedKing[0].textContent);
+window.alert(conceitedKing[1].textContent);
 
+// 5
 
+var noBusiness = document.querySelectorAll('.asteroid');
+console.log(noBusiness[0].textContent,noBusiness[1].textContent,noBusiness[2].textContent);
+
+// 6
+
+var allBizniss = document.querySelector('.big');
+console.log(allBizniss.textContent);
 
 
 
