f3899c75c1c5dc4310d8ff89a84817ad0fb0c2c8
Gabor V <gabor.w.varga@gmail.com>
2017-05-05 16:06:29 +0200

    feat(done) weekly task(html,css,js)

--- week-07/day-05/weekly.js
+++ week-07/day-05/weekly.js
@@ -1,38 +1,35 @@
 // 'use strict';
 
-// var imageList = [
-//       {
-//         img: 'img/pic1.jpg',
-//         title: 'Foxes',
-//         descripton: 'some description'
-//       },
-//       {
-//         img: 'img/pic2.jpg',
-//         title: 'Whales and Rabbits',
-//         descripton: 'some description'
-//       },
-//       {
-//         img: 'img/pic3.jpg',
-//         title: 'Baobabs and Roses',
-//         descripton: 'some description'
-//       },
-//       {
-//         img: 'img/pic4.jpg',
-//         title: 'Giant monsters',
-//         descripton: 'some descripton'
-//       },
-//       {
-//         img: 'img/pic5.jpg',
-//         title: 'Sheep',
-//         descripton: 'some description'
-//       }
-//     ]
+var imageList = [
+      {
+        img: 'img/pic1.jpg',
+        title: 'Foxes',
+        descripton: 'some description'
+      },
+      {
+        img: 'img/pic2.jpg',
+        title: 'Whales and Rabbits',
+        descripton: 'some description'
+      },
+      {
+        img: 'img/pic3.jpg',
+        title: 'Baobabs and Roses',
+        descripton: 'some description'
+      },
+      {
+        img: 'img/pic4.jpg',
+        title: 'Giant monsters',
+        descripton: 'some descripton'
+      },
+      {
+        img: 'img/pic5.jpg',
+        title: 'Sheep',
+        descripton: 'some description'
+      }
+    ]
 
 var clickList = document.querySelectorAll('.item');
 
-function methodName(arguments) {
-  
-}
 
 function slider (event) {
 
@@ -43,18 +40,23 @@ function slider (event) {
   if (imgClass.contains('one')) {
     imgUrl.setAttribute('src', 'img/pic1.jpg');
     imgCss[0].style.backgroundImage="url(img/pic1.jpg)"
+
   } else if (imgClass.contains('two')) {
     imgUrl.setAttribute('src', 'img/pic2.jpg');
     imgCss[1].style.backgroundImage="url(img/pic2.jpg)" 
+
   } else if (imgClass.contains('three')) {
     imgUrl.setAttribute('src', 'img/pic3.jpg');
     imgCss[2].style.backgroundImage="url(img/pic3.jpg)" 
+
   } else if (imgClass.contains('four')) {
     imgUrl.setAttribute('src', 'img/pic4.jpg');
     imgCss[3].style.backgroundImage="url(img/pic4.jpg)" 
+
   } else if (imgClass.contains('five')) {
     imgUrl.setAttribute('src', 'img/pic5.jpg');
-    imgCss[4].style.backgroundImage="url(img/pic5.jpg)" 
+    imgCss[4].style.backgroundImage="url(img/pic5.jpg)"
+
   } else if (imgClass.contains('six')) {
     imgUrl.setAttribute('src', 'img/pic6.jpg');
     imgCss[5].style.backgroundImage="url(img/pic6.jpg)" 
@@ -66,8 +68,14 @@ clickList.forEach(function(elem) {
 });
 
 
+var imgNum = 0;
+var defImage = document.querySelector('img');
+console.log(imageList[imgNum].img);
+defImage.setAttribute('img', imageList[imgNum].img);
 
-
+function newSlider(event) {
+  
+}
 
 
 
