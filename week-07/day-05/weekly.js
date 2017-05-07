af80b19b2a6c23d9d46bacc375131f8e909268ec
Gabor V <gabor.w.varga@gmail.com>
2017-05-08 01:19:41 +0200

    update weekly project feat(left andf right image slide works, refactored js code)

--- week-07/day-05/weekly.js
+++ week-07/day-05/weekly.js
@@ -1,81 +1,111 @@
-// 'use strict';
+'use strict';
 
-var imageList = [
-      {
-        img: 'img/pic1.jpg',
-        title: 'Foxes',
-        descripton: 'some description'
-      },
-      {
-        img: 'img/pic2.jpg',
-        title: 'Whales and Rabbits',
-        descripton: 'some description'
-      },
-      {
-        img: 'img/pic3.jpg',
-        title: 'Baobabs and Roses',
-        descripton: 'some description'
-      },
-      {
-        img: 'img/pic4.jpg',
-        title: 'Giant monsters',
-        descripton: 'some descripton'
-      },
-      {
-        img: 'img/pic5.jpg',
-        title: 'Sheep',
-        descripton: 'some description'
-      }
-    ]
+var clickList = document.querySelectorAll('.thumb'); 
+var leftButton = document.querySelector('.left');
+var rightButton = document.querySelector('.right');
+var imageIndex = Math.floor(Math.random() * imageList.length + 1);
+console.log(imageIndex);
+var mainImage = document.querySelector('.mainImage');
+mainImage.setAttribute('src', imageList[imageIndex].url);
 
-var clickList = document.querySelectorAll('.item');
+function slideRight() {
+    imageIndex ++;
+    // console.log(imageIndex);
+    // for (var i = 0; i < imageList.length; i++) {
+
+        mainImage.setAttribute('src', imageList[imageIndex].url);
+    // }
+    if (imageIndex === imageList.length - 1) {
+        imageIndex = 0;
+    };
+};
 
 
-function slider (event) {
+function slideLeft() {
+    imageIndex --;
+    // console.log(imageIndex);
+    // for (var i = 0; i < imageList.length; i++) {
 
-  var imgClass = event.target.classList;
-  var imgUrl = document.querySelector('img');
-  var imgCss = document.querySelectorAll('.item');
-
-  if (imgClass.contains('one')) {
-    imgUrl.setAttribute('src', 'img/pic1.jpg');
-    imgCss[0].style.backgroundImage="url(img/pic1.jpg)"
-
-  } else if (imgClass.contains('two')) {
-    imgUrl.setAttribute('src', 'img/pic2.jpg');
-    imgCss[1].style.backgroundImage="url(img/pic2.jpg)" 
-
-  } else if (imgClass.contains('three')) {
-    imgUrl.setAttribute('src', 'img/pic3.jpg');
-    imgCss[2].style.backgroundImage="url(img/pic3.jpg)" 
-
-  } else if (imgClass.contains('four')) {
-    imgUrl.setAttribute('src', 'img/pic4.jpg');
-    imgCss[3].style.backgroundImage="url(img/pic4.jpg)" 
-
-  } else if (imgClass.contains('five')) {
-    imgUrl.setAttribute('src', 'img/pic5.jpg');
-    imgCss[4].style.backgroundImage="url(img/pic5.jpg)"
-
-  } else if (imgClass.contains('six')) {
-    imgUrl.setAttribute('src', 'img/pic6.jpg');
-    imgCss[5].style.backgroundImage="url(img/pic6.jpg)" 
-  }
-}
-
-clickList.forEach(function(elem) {
-  elem.addEventListener('mouseover', slider);
-});
+        mainImage.setAttribute('src', imageList[imageIndex].url);
+    // }
+    if (imageIndex === 0) {
+        imageIndex = imageList.length - 1;
+    };
+};
 
 
-var imgNum = 0;
-var defImage = document.querySelector('img');
-console.log(imageList[imgNum].img);
-defImage.setAttribute('img', imageList[imgNum].img);
 
-function newSlider(event) {
+
+leftButton.addEventListener('click', slideLeft);
+rightButton.addEventListener('click', slideRight);
+
+
+// var defImage = document.querySelector('img');
+// console.log(imageList[imgNum]);
+// // defImage.setAttribute('img', imageList[imgNum].img);
+
+
+// function slider(arguments) {
   
-}
+// }
+
+
+// function contoller(event) {
+//   console.log(defImage);
+// };
+
+
+// clickList.forEach(function(elem) {
+//   elem.addEventListener('click', contoller);
+// });
+
+
+
+
+
+
+
+
+
+// function slider (event) {
+
+//   var imgClass = event.target.classList;
+//   var imgUrl = document.querySelector('img');
+//   // var imgCss = document.querySelectorAll('.item');
+
+//   if (imgClass.contains('one')) {
+//     imgUrl.setAttribute('src', 'img/pic1.jpg');
+//     clickList[0].style.backgroundImage="url(img/pic1.jpg)"
+
+//   } else if (imgClass.contains('two')) {
+//     imgUrl.setAttribute('src', 'img/pic2.jpg');
+//     clickList[1].style.backgroundImage="url(img/pic2.jpg)" 
+
+//   } else if (imgClass.contains('three')) {
+//     imgUrl.setAttribute('src', 'img/pic3.jpg');
+//     clickList[2].style.backgroundImage="url(img/pic3.jpg)" 
+
+//   } else if (imgClass.contains('four')) {
+//     imgUrl.setAttribute('src', 'img/pic4.jpg');
+//     clickList[3].style.backgroundImage="url(img/pic4.jpg)" 
+
+//   } else if (imgClass.contains('five')) {
+//     imgUrl.setAttribute('src', 'img/pic5.jpg');
+//     clickList[4].style.backgroundImage="url(img/pic5.jpg)"
+
+//   } else if (imgClass.contains('six')) {
+//     imgUrl.setAttribute('src', 'img/pic6.jpg');
+//     clickList[5].style.backgroundImage="url(img/pic6.jpg)" 
+//   }
+// }
+
+// var clickList = document.querySelectorAll('.item');
+
+// clickList.forEach(function(elem) {
+//   elem.addEventListener('mouseover', slider);
+// });
+
+  
 
 
 
---
+++ week-07/day-05/weekly_database.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var imageList = [
+      {
+        url: 'img/pic1.jpg',
+        title: 'Foxes',
+        descripton: 'some description'
+      },
+      {
+        url: 'img/pic2.jpg',
+        title: 'Whales and Rabbits',
+        descripton: 'some description'
+      },
+      {
+        url: 'img/pic3.jpg',
+        title: 'Baobabs and Roses',
+        descripton: 'some description'
+      },
+      {
+        url: 'img/pic4.jpg',
+        title: 'Giant monsters',
+        descripton: 'some descripton'
+      },
+      {
+        url: 'img/pic5.jpg',
+        title: 'Sheep',
+        descripton: 'some description'
+      },
+       {
+        url: 'img/pic6.jpg',
+        title: 'Sheep',
+        descripton: 'some description'
+      }
+    ];
