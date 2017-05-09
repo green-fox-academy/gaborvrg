b5b420676c724f64bb7a29a47c235b6c64801cf7
Gabor V <gabor.w.varga@gmail.com>
2017-05-09 13:27:14 +0200

    added cars.js 2/3

---
+++ week-08/day-02/test.js
@@ -0,0 +1,12 @@
+function f() {
+  return this.a;
+}
+
+var g = f.bind({a: 'azerty'});
+console.log(g()); // azerty
+
+var h = g.bind({a: 'yoo'}); // bind only works once!
+console.log(h()); // azerty
+
+var boundF = f.bind("foo");
+boundF("hello", "world");
