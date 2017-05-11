3101318079a33f33be4c65be7caa05521e2d2eb2
Gabor V <gabor.w.varga@gmail.com>
2017-05-11 17:49:40 +0200

    finished tesk2

--- week-08/day-04/nytimesdevapi.js
+++ week-08/day-04/nytimesdevapi.js
@@ -1,16 +1,56 @@
 // API Key for the Article Search API: 6634bd1387474f6c95b27a843981d522
 
-var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json&api_key=6634bd1387474f6c95b27a843981d522'; //A local page
+var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=moon+landing+by+Apollo+11&api-key=6634bd1387474f6c95b27a843981d522'; 
+
 
 function load(url, callback) {
   var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-      console.log(xhr.response); //Outputs a DOMString by default
+        let responseFromServer = JSON.parse(xhr.response);  // IMPORTANT
+        // console.log(responseFromServer.response.docs);
+        callback(responseFromServer.response.docs);
     }
-  }
+  };
 
   xhr.open('GET', url, true);
   xhr.send('');
 }
+
+load(url, function(datas) {
+    var newDiv = document.createElement('div'); 
+    var newDl = document.createElement('dl');
+    document.querySelector('body').appendChild(newDiv).className = 'content';
+    document.querySelector('.content').appendChild(newDl).className = 'section';
+
+    datas.forEach(function(elem, idx) {
+        // console.log(elem);
+        // console.log(elem.snippet);
+        // console.log(elem.pub_date);
+        var newDt = document.createElement('dt');
+        newDt.className = 'header' + (idx+1);
+        newDt.textContent = elem.headline.main;
+        document.querySelector('.section').appendChild(newDt);
+
+        var newDd = document.createElement('dd');
+        newDd.className = 'snippet';
+        newDd.textContent = elem.snippet;
+        document.querySelector('.header' + (idx+1)).appendChild(newDd);
+
+        var newDd2 = document.createElement('dd');
+        newDd2.className = 'pub_date';
+        newDd2.textContent = elem.pub_date;
+        document.querySelector('.header' + (idx+1)).appendChild(newDd2);
+
+        var a = document.createElement('a');
+        a.textContent = 'Permalink';
+        a.href = elem.web_url;
+        document.querySelector('.header' + (idx+1)).appendChild(a);
+
+    });
+});
+
+
+
+
