4cedb38b6443512833651eef5f66cc1b360838cd
Gabor V <gabor.w.varga@gmail.com>
2017-05-16 14:54:23 +0200

    added greeter and it works

--- week-09/day-02/frontend/server.js
+++ week-09/day-02/frontend/server.js
@@ -25,8 +25,27 @@ app.get('/doubling', function(req, res) {
 });
 
 
-// app.listen(8080);
+app.get('/greeter', function (req, res) {
+    // console.log(req.query.name + " " + req.query.title);
+    if (req.query.name === undefined) {
+        res.send({
+            error: "Please provide a name!"
+                });
+    } else if (req.query.title === undefined) {
+        res.send({
+            error: "Please provide a title!"
+                });
+    } else if (req.query.name === "Petike" && req.query.title === "student"){
+        res.send({
+            welcome_message: "Oh, hi there " + req.query.name + " , my dear " + req.query.title + " !"
+                });
+    }
+});
 
+
+
+
+// app.listen(8080);
 app.listen(8080, function() {
     console.log('server is running');
 });
