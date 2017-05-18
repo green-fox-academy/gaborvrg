1288209c424831ec31c9ce1ccc8f083b49a32c53
Gabor V <gabor.w.varga@gmail.com>
2017-05-18 13:52:43 +0200

    added All books with full data part of today workshop

--- week-09/day-04/index.js
+++ week-09/day-04/index.js
@@ -11,6 +11,65 @@ var conn = mysql.createConnection({
   database: "bookstore"
 });
 
+
+app.get('/', function get(req, res) {
+    conn.query(`SELECT book_mast.book_name ,author.aut_name, category.cate_descrip, publisher.pub_name, book_mast.book_price FROM book_mast
+INNER JOIN author ON book_mast.aut_id=author.aut_id
+INNER JOIN category ON book_mast.cate_id=category.cate_id
+INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id; `,function(err, rows){
+    var table = "<table>";
+    table += "<tr><th>" + "Book name" + "</th><th>" + "Author" + "</th><th>" + "Category" + "</th><th>" + "Publisher Name" + "</th><th>" + "Book Price" + "</th></tr>";
+    rows.forEach(function(record) {
+        // console.log(rows);
+        table += "<tr>";
+        table += "<td>" + record.book_name + "</td>";
+        table += "<td>" + record.aut_name + "</td>";
+        table += "<td>" + record.cate_descrip + "</td>";
+        table += "<td>" + record.pub_name + "</td>";
+        table += "<td>" + record.book_price + "</td>";
+        table += "</tr>";
+        });
+    table += "</table>";
+    res.send(table);
+    });
+});
+
+// app.get('/', function get(req, res) {
+//     conn.query("SELECT book_name FROM book_mast;",function(err, rows){
+//     var html = "<ul>";
+//     if(err) {
+//         console.log('PARA', err);
+//     } else {
+//       rows.forEach(function(record) {
+//           console.log(record.book_name);
+//           html += "<li>" + record.book_name + "</li>";
+//             });
+//         }
+//       html += "</ul>";
+//       res.send(html);
+//     });
+// });
+
+
+app.listen(3000, function() {
+    console.log('server is running');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // conn.query("SELECT * FROM author;",function(err,rows){
 //   console.log("Data received from Db:\n");
 //   rows.forEach(function(elem) {
@@ -68,17 +127,6 @@ var conn = mysql.createConnection({
 //     });
 // });
 
-app.get('/', function get(req, res) {
-    conn.query("SELECT book_name FROM book_mast;",function(err, rows){
-      var html = "<ul>";
-      rows.forEach(function(record) {
-          console.log(record.book_name);
-          html += "<li>" + record.book_name + "</li>";
-      });
-      html += "</ul>"
-      res.send(html);
-    });
-});
 
 
 // conn.query("SELECT book_name FROM book_mast;",function(err,rows){
@@ -91,9 +139,6 @@ app.get('/', function get(req, res) {
 // });
 
 
-app.listen(3000, function() {
-    console.log('server is running');
-});
 
 
 // conn.query(query, function(err,rows){
