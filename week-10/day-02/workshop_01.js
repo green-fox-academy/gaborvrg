9691abfcfcfd5f0d57edf6b270d89862832209df
Gabor V <gabor.w.varga@gmail.com>
2017-05-23 13:13:39 +0200

    update workshop1 execrcise with prototype

--- week-10/day-02/workshop_01.js
+++ week-10/day-02/workshop_01.js
@@ -5,15 +5,52 @@
 // Every animal should have a method called say() that prints what the animal says
 
 
-function Animals(sound){
+// function Animals(sound){
+//     this.sound = sound;
+//     this.say = function(){
+//         console.log(this.sound);
+//     };
+// }
+
+// var cow = new Animals('muuu');
+// cow.say();
+
+// var dog = new Animals('wuff');
+// dog.say();
+
+// ****************************
+// with prototype
+
+function Animals(sound) {
     this.sound = sound;
-    this.say = function(){
-        console.log(this.sound);
-    };
 }
 
-var cow = new Animals('muuu');
-cow.say();
+Animals.prototype.print = function () {
+    console.log(this.sound);
+};
+
+var cow = new Animals("muuua");
+cow.print();
 
 var dog = new Animals('wuff');
-dog.say();
+dog.print();
+
+
+// function Car(km) {
+//  this.km = km;
+// }
+
+// Car.prototype.ride = function(km) {
+//  this.km += km;
+// }
+
+// var volvo = new Car(80000);
+// volvo.ride(120);
+// console.log(volvo.km); // 80120
+
+
+
+
+
+
+
