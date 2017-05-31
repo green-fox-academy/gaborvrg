f99e869a5a882a087e7baa8aa3c7ce274f814255
Gabor V <gabor.w.varga@gmail.com>
2017-05-31 16:46:32 +0200

    giphy.js(feat: search bar finally works)

--- week-08/day-04/giphyapi.js
+++ week-08/day-04/giphyapi.js
@@ -4,7 +4,6 @@ var keyword = document.querySelector('.keyword');
 
 function load(url, callback) {
   var xhr = new XMLHttpRequest();
-
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       let responseFromServer = JSON.parse(xhr.response);  // IMPORTANT
@@ -12,43 +11,30 @@ function load(url, callback) {
       callback(responseFromServer);
     }
   };
-
   xhr.open('GET', url, true);
   xhr.send('');
 }
 
-
-load(url, function(datas){
-    datas.data.forEach(function(elem, idx) {
-        console.log(elem);
-        document.querySelector('.item' + (idx+1)).style.backgroundImage = 'url(\'' + elem.images.downsized_still.url + '\')';
-        document.querySelector('.item' + (idx+1)).addEventListener('mouseover', function(e) {
-            console.log(elem);
-            document.querySelector('.item' + (idx+1)).style.backgroundImage = 'url(\'' + elem.images.downsized.url + '\')';
-        });
-    });
-});
-
-
-
 function getkeyword(keyword) {
-    var keywordWithSpaces = keyword.replace(/ /g ,'+'); // https://goo.gl/NmuEVs
-    const endpoint = 'http://api.giphy.com/v1/gifs/search?q='+ keywordWithSpaces + '&api_key=dc6zaTOxFJmzC&limit=16';
-    console.log(endpoint);
-    load(endpoint);
+  var keywordWithSpaces = keyword.replace(/ /g ,'+'); // https://goo.gl/NmuEVs
+  const endpoint = 'http://api.giphy.com/v1/gifs/search?q='+ keywordWithSpaces + '&api_key=dc6zaTOxFJmzC&limit=16';
+
+  load(endpoint, function(datas){
+    datas.data.forEach(function(elem, idx) {
+      document.querySelector('.item' + (idx+1)).style.backgroundImage = 'url(\'' + elem.images.downsized_still.url + '\')';
+      document.querySelector('.item' + (idx+1)).addEventListener('click', function(e) {
+        document.querySelector('.item' + (idx+1)).style.backgroundImage = 'url(\'' + elem.images.downsized.url + '\')';
+      });
+    });
+  });
 }
 
 
-
-
 keyword.addEventListener('keyup', function(e) {
-    if(e.keyCode == 13) { // Enter key
-        // console.log(keyword.value);
-        getkeyword(keyword.value);
-    }
+  if(e.keyCode == 13) { // Enter key
+      getkeyword(keyword.value);
+  }
 });
 
-
-
-
+getkeyword('cat')
 
