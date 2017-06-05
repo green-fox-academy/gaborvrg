2e348d6e054d955adfbf3d1e4a1e16533e7dc9f3
Gabor V <gabor.w.varga@gmail.com>
2017-06-05 15:42:44 +0200

    todo(feat: callback problem fixed )

--- week-10btwn11/todo/js/controller.js
+++ week-10btwn11/todo/js/controller.js
@@ -8,10 +8,13 @@ let newAjax = ajax.load;
 let addButton = document.querySelector('.add-button');
 let addTodo = document.querySelector('.add-todo');
 
+let callback = 	function () {
+	newAjax('Get', url + 'todos', addTodo.value, render);
+};
+
 
 addButton.addEventListener('click', function(){
-	newAjax('Post', url + 'add-todos', addTodo.value);
-	newAjax('Get', url + 'todos', addTodo.value, render);
+	newAjax('Post', url + 'add-todos', addTodo.value, callback);
 	addTodo.value = '';
 });
 
--- week-10btwn11/todo/server.js
+++ week-10btwn11/todo/server.js
@@ -40,7 +40,6 @@ app.get('/todos', function(req, res) {
   });
 });
 
-
 app.post('/add-todos', function(req, res) {
   console.log(req.body.todoText);
   conn.query('INSERT INTO todolist (text) VALUES ("' + req.body.todoText + '")', function(err, rows){ 
@@ -48,23 +47,10 @@ app.post('/add-todos', function(req, res) {
       console.log(err.toString());
       return;
     }
-    // console.log(rows);
-    // res.send(rows);
+    res.send(rows);
   });
 });
 
-// app.post('/add-todos', function(req, res) {
-//   console.log(req.body.todoText);
-//   conn.query('INSERT INTO todolist (text) VALUES ("' + req.body.todoText + '")', function(err, rows){ 
-//     conn.query('SELECT * FROM todolist', function(err,rows){
-//       if(err) {
-//         console.log(err.toString());
-//         return;
-//       }
-//       res.send(rows);
-//     });
-//   });
-// });
 
 app.listen(3000, function() {
 	console.log('server is running');
