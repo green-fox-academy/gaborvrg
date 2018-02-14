5d6c6d2cb93abb77b2449e9dc9c5d60d8d397851
Varga Gábor <varga.gabor@ad.mikrum.hu>
2018-02-14 09:34:13 +0100

    added exception conditional

--- Final exam/final_exam.js
+++ Final exam/final_exam.js
@@ -5,10 +5,14 @@
 let reversedString = '';
 
 let reverseAString = function(str) {
-  for (let i = str.length - 1; i>=0; i--) {
-    reversedString += str[i];
-  }
+  if (typeof str !== 'string') {
+    return 'Error: input is not a string';
+  } else {
+    for (let i = str.length - 1; i>=0; i--) {
+      reversedString += str[i];
+    }
   return reversedString;
+  }
 };
 
 console.log(reverseAString('string for reverse'));
